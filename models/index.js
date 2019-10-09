ddb91b080f3c154ba7d1e1798207cd3b4ceea701
daltonhart <daltonvaren@gmail.com>
2019-10-09 09:30:03 -0700

    adds refactor to model file

--- models/index.js
+++ models/index.js
@@ -1,2 +1,11 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/book-app");
+const DB_URL = "mongodb://localhost:27017/book-app";
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true 
+})
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.log(err));
